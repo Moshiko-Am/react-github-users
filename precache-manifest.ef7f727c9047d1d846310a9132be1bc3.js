0a38a1ef41c57f068b6164b1460ef1df56a909c9
Moshiko-Start <moshiko14691@gmail.com>
2021-12-05 12:47:05 +0200

    Updates

---
+++ precache-manifest.ef7f727c9047d1d846310a9132be1bc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12d9ee0d099291a1f9a3022ed44520f",
+    "url": "/react-github-users/index.html"
+  },
+  {
+    "revision": "1e79777871b613578a1a",
+    "url": "/react-github-users/static/css/main.b854109b.chunk.css"
+  },
+  {
+    "revision": "fd57b11ac1ec1706a87b",
+    "url": "/react-github-users/static/js/2.8e479733.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/react-github-users/static/js/2.8e479733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e79777871b613578a1a",
+    "url": "/react-github-users/static/js/main.477b9531.chunk.js"
+  },
+  {
+    "revision": "42a4a8ee82850bab2628",
+    "url": "/react-github-users/static/js/runtime-main.b0ba463c.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/react-github-users/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/react-github-users/static/media/preloader.c114fda3.gif"
+  }
+]);
